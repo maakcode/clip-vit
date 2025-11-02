a1b6a3bb71bb903ffaa42f53fa44074d48446536
MaakCode <11623298+maakcode@users.noreply.github.com>
2025-11-02 19:14:25 +0900

    clip-ViT-B-32-multilingual-v1 기반으로 변경

--- main.py
+++ main.py
@@ -1,61 +1,37 @@
 import os
-from typing import Tuple
 import streamlit as st
+from PIL import Image
+from sentence_transformers import SentenceTransformer
 import faiss
-from PIL import Image, ImageFile
-from mlx_clip import CLIPModel, CLIPTokenizer, CLIPImageProcessor, convert_clip
-from mlx_lm.generate import generate as mlx_generate
-from mlx_lm.convert import convert as mlx_convert
-from mlx_lm.utils import load as mlx_load
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
 
 class ClipImageSearch:
     def __init__(self):
-        model_name = "openai/clip-vit-large-patch14"
-        model_path = f"./models/{model_name}"
-
+        image_model_name = "clip-ViT-B-32"
+        text_model_name = "sentence-transformers/clip-ViT-B-32-multilingual-v1"
         self.index = None
         self.image_names = []
         self.image_objects = []
 
-        st.write(f"🚀 CLIP 모델 로딩 중... ({model_name})")
-        self.model, self.tokenizer, self.img_processor = self._load(
-            model_name, model_path
-        )
+        st.write(f"🚀 CLIP 모델 로딩 중... ({image_model_name})")
+        self.image_model = SentenceTransformer(image_model_name)
+        self.text_model = SentenceTransformer(text_model_name)
         st.success("✅ CLIP 모델 로드 완료!")
 
-    def _load(
-        self, model_name: str, model_path: str
-    ) -> Tuple[CLIPModel, CLIPTokenizer, CLIPImageProcessor]:
-        if not os.path.isdir(model_path):
-            convert_clip(model_name, model_path)
-
-        model = CLIPModel.from_pretrained(model_path)
-        tokenizer = CLIPTokenizer.from_pretrained(model_path)
-        img_processor = CLIPImageProcessor.from_pretrained(model_path)
-        return model, tokenizer, img_processor
-
-    def _get_image_embeddings(self, images: list[ImageFile.ImageFile]):
+    def _get_image_embeddings(self, images: list[Image.Image]):
         """여러 이미지의 임베딩을 계산합니다."""
-        rgb_images = [img.convert("RGB") for img in images]
-        inputs = {
-            "pixel_values": self.img_processor(rgb_images),
-        }
-        return self.model(**inputs).image_embeds
+        return self.image_model.encode(images, normalize_embeddings=True)
 
     def _get_text_embeddings(self, texts: list[str]):
         """여러 텍스트의 임베딩을 계산합니다."""
-        inputs = {
-            "input_ids": self.tokenizer(texts),
-        }
-        return self.model(**inputs).text_embeds
+        return self.text_model.encode(texts, normalize_embeddings=True)
 
     def build_index(self, uploaded_files):
         """업로드된 이미지로 FAISS 인덱스 생성"""
         self.image_names = [f.name for f in uploaded_files]
-        self.image_objects = [Image.open(f) for f in uploaded_files]
+        self.image_objects = [Image.open(f).convert("RGB") for f in uploaded_files]
 
         embeddings = self._get_image_embeddings(self.image_objects)
 
@@ -80,43 +56,6 @@ class ClipImageSearch:
         return results
 
 
-class LLMTranslator:
-    def __init__(self):
-        model_name = "ibm-granite/granite-4.0-h-micro"
-        model_path = f"./models/{model_name}"
-
-        st.write(f"🚀 언어 모델 로딩 중... ({model_name})")
-        self._convert(model_name, model_path)
-        self.model, self.tokenizer, *rest = mlx_load(model_path)
-        st.success("✅ 언어 모델 로드 완료!")
-
-    def _convert(self, model_name: str, model_path: str):
-        if os.path.isdir(model_path):
-            return
-
-        mlx_convert(model_name, quantize=True, mlx_path=model_path)
-
-    def translate(self, text: str) -> str:
-        chat = [
-            {
-                "role": "system",
-                "content": "You are a translator. Translate the given text into English only, without any additional comments or responses.",
-            },
-            {
-                "role": "user",
-                "content": text,
-            },
-        ]
-
-        prompt = self.tokenizer.apply_chat_template(chat, add_generation_prompt=True)
-        text = mlx_generate(
-            self.model,
-            self.tokenizer,
-            prompt=prompt,
-        )
-        return text
-
-
 def main():
     # ============================================
     # Streamlit UI
@@ -130,12 +69,7 @@ def main():
     def load_app():
         return ClipImageSearch()
 
-    @st.cache_resource
-    def load_translator():
-        return LLMTranslator()
-
     app = load_app()
-    translator = load_translator()
 
     # 파일 업로드
     uploaded_files = st.file_uploader(
@@ -163,9 +97,7 @@ def main():
         top_k = st.slider("검색할 상위 이미지 개수", 1, 10, 3)
 
         if st.button("🔎 검색 실행"):
-            translated_query = translator.translate(query)
-            st.write(f"{query} {translated_query}")
-            results = app.search(translated_query, top_k)
+            results = app.search(query, top_k)
             if results:
                 st.write("### 검색 결과:")
                 cols = st.columns(top_k)
--- mlx_clip/__init__.py
+++
@@ -1,4 +0,0 @@
-from .image_processor import CLIPImageProcessor
-from .model import CLIPModel
-from .tokenizer import CLIPTokenizer
-from .convert import convert as convert_clip
